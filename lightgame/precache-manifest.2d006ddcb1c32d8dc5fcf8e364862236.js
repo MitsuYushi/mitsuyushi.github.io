3a551be2a5bb9e085d18c9c95ae1dc4740f7c9c6
MitsuYushi <teavsothearath@gmail.com>
2021-03-26 23:03:47 +0700

    lightgame

---
+++ lightgame/precache-manifest.2d006ddcb1c32d8dc5fcf8e364862236.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6bdf1c54ba0925273136",
+    "url": "/static/css/main.308aaf39.chunk.css"
+  },
+  {
+    "revision": "6bdf1c54ba0925273136",
+    "url": "/static/js/main.6bdf1c54.chunk.js"
+  },
+  {
+    "revision": "b43a7eab4c27c21e54e5",
+    "url": "/static/js/1.b43a7eab.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c8d22a22ef9054ad0b49c3f46844ff51",
+    "url": "/index.html"
+  }
+];
